74844d9d2448fc2929bd601652186f4c6f784684
sunwin1029 <sunwin1029@gmail.com>
2026-04-16 17:44:33 +0900

    fix: Rating 생성자 수정

--- Rating.cpp
+++ Rating.cpp
@@ -11,13 +11,12 @@ using namespace std;
 //     }
 
 // 범위 검사를 위해 초기화 리스트 대신, 생성자를 직접 정의하였습니다.
-Rating::Rating(int userId, int movieId, double score) {
-    if(score < 0.0 || score > 5.0) {
+Rating::Rating(int userId, int movieId, double score) :
+userId(userId), movieId(movieId), score(score) {
+    if(this->score < 0.0 || this->score > 5.0) {
         cout << "잘못된 점수 범위가 입력되었습니다! 점수가 0으로 설정됩니다\n";
-        score = 0.0;
+        this->score = 0.0;
     }
-    userId = userId;
-    movieId = movieId;
 }
 
 int Rating::getUserId() const { return userId; }
